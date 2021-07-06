c944fb015067c2bce79dd3b7f45561419caa6f96
Helena <h.abneymcp@gmail.com>
2021-07-06 01:56:33 -0500

    create lists of notes and durations in the melodies

--- melody_generation.py
+++ melody_generation.py
@@ -5,3 +5,37 @@
 # generate is "melodic," then running this method will be the first step.
 
 from music21 import *
+import os
+
+save_dir = 'melodies/'
+
+melodies = os.listdir(save_dir)
+scores = []
+
+for melody in melodies:
+    score = converter.parse(save_dir + melody)
+    scores.append(score)
+
+# scores[0].show('text')
+
+notes = [[] for score in scores]
+durations = [[] for score in scores]
+keys = []
+
+for i, melody in enumerate(scores):
+    # print(str(i))
+    keys.append(str(melody.analyze('key')))
+    for element in melody:
+        if isinstance(element, stream.Part):
+            for measure in element:
+                if isinstance(measure, stream.Measure):
+                    for thing in measure:
+                        # print(element)
+                        if isinstance(thing, note.Note):
+                            notes[i].append(thing.pitch)
+                            durations[i].append(thing.duration.quarterLength)
+                        # In this case, since all the melodies are a single line,
+                        # every note is a Note and not a Chord.
+
+# print(notes)
+# print(durations)
