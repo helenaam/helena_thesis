f78ecd0d388e2fd9b38dbffcd873c3c9e2a559e0
Helena <h.abneymcp@gmail.com>
2021-07-23 16:33:18 -0500

    Tested creation of chord bank

--- generate_chords.py
+++ generate_chords.py
@@ -11,27 +11,25 @@ scores = []
 
 for piece in chords:
     score = converter.parse(save_dir + piece)
+    # These next few lines are specific to the input pieces I was using to test
+    # this program, since music21 was misidentifying the keys of these pieces.
+    if "eminor" in piece:
+        score.keySignature = key.Key('E', 'minor')
+    elif "aminor" in piece:
+        score.keySignature = key.Key('A', 'minor')
+    elif "cminor" in piece:
+        score.keySignature = key.Key('C', 'minor')
+    elif "fmajor" in piece:
+        score.keySignature = key.Key('F', 'major')
+    elif "bflatmajor" in piece:
+        score.keySignature = key.Key('B-', 'major')
+    elif "dminor" in piece:
+        score.keySignature = key.Key('D', 'minor')
+    elif "cmajor" in piece:
+        score.keySignature = key.Key('C', 'major')
     scores.append(score)
 
-# Transpose everything into C major or C minor
-
-# majorpieces = []
-# minorpieces = []
-# transposed_scores = []
-
-# for i, piece in enumerate(scores):
-#     key = piece.analyze('key')
-#     k = str(key)
-#     interv = interval.Interval(key.tonic, pitch.Pitch('C'))
-#     transposed = piece.transpose(interv)
-#     piece_chords = transposed.chordify()
-#     if "major" in k:
-#         majorpieces.append(piece_chords)
-#     if "minor" in k:
-#         minorpieces.append(piece_chords)
-#     transposed_scores.append(piece_chords)
-
-
+    
 # The "melody" input is a music21.stream.Score object.
 # The "key" input is a music21.key.Key object.
 def generateChords(melody, key):
@@ -40,8 +38,18 @@ def generateChords(melody, key):
     transposed_scores = []
     k = str(key)
     for piece in scores:
-        start_key = piece.analyze('key')
+        # Commented this line out because it was incorrectly determining the keys
+        # of my inputs
+        # start_key = piece.analyze('key')
+        
+        # These next few lines are specific to this set of input works that I was
+        # using to test the program.
+        # print(piece)
+        start_keys = piece.getKeySignatures()
+        start_key = start_keys[0]
+        
         k_start = str(start_key)
+        # print(k_start)
         if "major" in k_start and "major" in k or "minor" in k_start and "minor" in k:
             interv = interval.Interval(start_key.tonic, key.tonic)
             transposed = piece.transpose(interv)
@@ -51,6 +59,7 @@ def generateChords(melody, key):
     # Get a list of all the unique chords in the works that share a mode
     # (major/minor) with this melody.
     unique_chords = []
+    roman_numerals = []
     for piece in transposed_scores:
         for c in piece_chords.recurse().getElementsByClass('Chord'):
             # Put them all in closed position and in the same octave so that
@@ -58,6 +67,21 @@ def generateChords(melody, key):
             # multiple different chords.  Different inversions still count as
             # "different" chords.
             c.closedPosition(forceOctave = 4, inPlace = True)
+            
             # Add the chord to the list if it's not already there.
-            if c not in unique_chords:
+            # if c not in unique_chords:
+            #     unique_chords.append(c)       
+            rn = roman.romanNumeralFromChord(c, key)
+            if rn not in roman_numerals:
+                roman_numerals.append(rn)
                 unique_chords.append(c)
+
+    # print(unique_chords)
+    # print(roman_numerals)
+    for chord in unique_chords:
+        print(chord)
+
+# Create a melody to test the generateChords function
+melody = converter.parse('melodies/melody1.mxl')
+# print(melody.analyze('key'))
+generateChords(melody, melody.analyze('key'))
